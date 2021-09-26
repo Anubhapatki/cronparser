3a33aa8366b3b172c0064a9e9e72043e39039d68
Anubha Vijayvargiya <anubha.vijayvargiya@tesco.com>
2021-09-27 00:47:27 +0100

    Improved unit test

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import unittest
-from main import cron_parser
-
+from parser import parse_string
 
 class TestCronParser(unittest.TestCase):
-    def test_cron_parser(self):
+    def test_cron_parser_interval(self):
         """
-        Test error handling
+        Test 
         """
-        return_results = 'Minutes 6 7 8 9'
-        result = cron_parser('*/15 0 1,15 * 1-9 /usr/bin/find')
-        self.assertEqual(return_results,'Minutes 6 7 8 9')
+        result = parse_string('*/15', 'minute')
+        self.assertEqual(result,'0 15 30 45')
+    def test_cron_parser_all(self):
+        """
+        Test 
+        """
+        result = parse_string('*', 'hour')
+        self.assertEqual(result,'0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23')
 
 if __name__ == '__main__':
     unittest.main()
